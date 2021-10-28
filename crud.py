eb7f9a1b004d21dfe2a95c9829d014e9b3901ac4
Virginia Lopez Nadal <vlopeznadal@icloud.com>
2021-10-28 16:28:26 -0500

    Finished up to View List of All Movies Task 1

--- crud.py
+++ crud.py
@@ -23,6 +23,10 @@ def create_movie(title, overview, release_date, poster_path):
 
     return movie
 
+def get_all_movies():
+    return  Movie.query.all()
+
+
 def create_rating(user, movie, score):
     """Create and return a new rating"""
     rating = Rating(user = user, movie = movie, score = score)
